9492baec245d78636758abdb9e6217ef18021874
JAYANTI SRIVASTAVA <JAYANTI SRIVASTAVA>
2016-12-29 22:32:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/card/card/Menu.java
@@ -0,0 +1,15 @@
+package com.example.card.card;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by JAYANTI SRIVASTAVA on 29-12-2016.
+ */
+public class Menu extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+    }
+}
